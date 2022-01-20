e4eafe03333d5d3c9f7c2ba9684b65385afff360
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2022-01-20 12:11:51 -0800

    added docstrings to methods

--- ascii.py
+++ ascii.py
@@ -7,7 +7,15 @@ np.set_printoptions(threshold=sys.maxsize)
 ASCII_VALUES = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,^. "
 
 
-def convert_ascii(img_arr, img_size):
+def convert_ascii(img_arr: list, img_size: tuple):
+    '''Converts a 1D image array into a 2D array of ASCII characters
+
+    Args:
+        img_arr (list): greyscale array of image
+        img_size (tuple): the size of the image
+    Returns:
+        ndarray: 2D ascii array of image
+    '''
     for i in range(len(img_arr)):
         char = math.floor(img_arr[i] * ((len(ASCII_VALUES)-1)/255))
         img_arr[i] = ASCII_VALUES[char]
@@ -15,14 +23,19 @@ def convert_ascii(img_arr, img_size):
     return np.reshape(img_arr, img_size)
 
 
-def print_drawing(ascii_arr):
+def print_drawing(ascii_arr: np.ndarray):
+    '''Prints a 2D ASCII array into the console
+
+    Args:
+        ascii_arr (ndarray)
+    '''
     for row in ascii_arr:
         line = ''
         for pixel in row:
             line += pixel
         print(line)
         
-        
+
 if __name__ == '__main__':
     file = '.\\ascii-converter\\test.jpg'
     image = Image.open(file)
