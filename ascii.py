43f93618063d76e835c88cb04bd2c0863cd22d82
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2022-01-20 13:52:29 -0800

    added webcam rendering

--- ascii.py
+++ ascii.py
@@ -2,17 +2,22 @@ from PIL import Image
 import math
 import numpy as np
 import sys
+import cv2 as cv
+import os
+import time
 
 np.set_printoptions(threshold=sys.maxsize)
-ASCII_VALUES = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,^. "
+#ASCII_VALUES = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,^. "
+ASCII_VALUES = " .^,:;Il!i><~+_-?][}{1)(|\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+IMG_SIZE = (128, 128)
+FRAME_RATE = 15
 
 
-def convert_ascii(img_arr: list, img_size: tuple):
+def convert_ascii(img_arr: list):
     '''Converts a 1D image array into a 2D array of ASCII characters
 
     Args:
         img_arr (list): greyscale array of image
-        img_size (tuple): the size of the image
     Returns:
         ndarray: 2D ascii array of image
     '''
@@ -20,7 +25,7 @@ def convert_ascii(img_arr: list, img_size: tuple):
         char = math.floor(img_arr[i] * ((len(ASCII_VALUES)-1)/255))
         img_arr[i] = ASCII_VALUES[char]
         
-    return np.reshape(img_arr, img_size)
+    return np.reshape(img_arr, IMG_SIZE)
 
 
 def print_drawing(ascii_arr: np.ndarray):
@@ -36,14 +41,49 @@ def print_drawing(ascii_arr: np.ndarray):
         print(line)
         
 
+def ascii_video():
+    prev = 0
+    cap = cv.VideoCapture(0)
+    if not cap.isOpened():
+        raise IOError("Cannot open webcam")
+
+    while True:
+        time_elapsed = time.time() - prev
+        ret, frame = cap.read()
+        
+        if time_elapsed > 1/FRAME_RATE:
+            prev = time.time()
+            frame = cv.resize(frame, IMG_SIZE)
+            cv.imshow('Input', frame)
+
+            greyscale = Image.fromarray(frame).convert('L')
+            data = list(greyscale.getdata(0))
+            
+            ascii_arr = convert_ascii(data)
+            print_drawing(ascii_arr)
+
+        #os.system('cls')
+            
+        # Key to exit is Escape
+        exitKey = cv.waitKey(1)
+        if exitKey == 27:
+            break
+        
+    cap.release()
+    cv.destroyAllWindows()
+
+
 if __name__ == '__main__':
+    '''
     file = '.\\ascii-converter\\test.jpg'
     image = Image.open(file)
-    size = (64, 64)
-    resized = image.resize(size)
+    resized = image.resize(IMG_SIZE)
     greyscale = resized.convert('L')
     data = list(greyscale.getdata(0))
     
-    ascii_arr = convert_ascii(data, size)
+    ascii_arr = convert_ascii(data)
     print_drawing(ascii_arr)
+    '''
+    
+    ascii_video()
     
